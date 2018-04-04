049ed0fb801e8641cb80ecf225fa854995c87f59
Dmitry Tokarev <dm.tokarev@yahoo.com>
2018-04-04 12:22:28 -0700

    changed initialization of driver object to be in params

--- conftest.py
+++ conftest.py
@@ -3,10 +3,10 @@ from selenium import webdriver
 
 
 @pytest.fixture(scope='session',
-                params=[webdriver.Chrome, webdriver.Firefox],
+                params=[webdriver.Chrome(), webdriver.Firefox()],
                 ids=['Chrome', 'Firefox'])
 def driver(request):
     # Create a new instance of a driver
-    driver = request.param()
+    driver = request.param
     yield driver
     driver.quit()
